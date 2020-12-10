3a87cde47848e306a582dd1a84b6a3bcd13831e4
dandy-collaborator-4866 <brobertson@utexas.edu>
2020-12-10 14:45:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 // add solution here
 function theBeatlesPlay(musicians, instruments) {
   var musicians = [];
-  
+
+  function incrementVariable() {
+  let i=0;
+  i=i+1;
+  return i;
+}
+do {
+if(num === 0) {
+console.log("I run once regardless.");
+} else if (num === 10){
+console.log("I run once regardless.")
+} while (incrementVariable() < num);
+}
+}
 }
