3cddabb848414f0b326442bc71ba22a2a4e3e5cd
dandy-collaborator-4866 <brobertson@utexas.edu>
2020-12-10 14:59:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,3 +9,14 @@ for (let i=0; i < musicians.length; i++){
 }
 return array;
 }
+
+const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
+
+function johnLennonFacts(array){
+  
+}
